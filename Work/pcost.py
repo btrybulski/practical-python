03730c4137e50f7f0e678484e5c0fe3eabfd5c92
Ben Trybulski <btrybulski1@gmail.com>
2020-10-17 11:11:31 -0400

    complete pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,19 @@
 # pcost.py
 #
 # Exercise 1.27
+
+def portfolio_cost(filename):
+    total_cost = 0
+    with open(filename, 'rt') as file: 
+        # csv heading - stock name, number of shares, purchase price
+        headers = next(file)
+        for line in file: 
+            row = line.split(',')
+            try: 
+                ticker, shares, price = row[0], int(row[1]), float(row[2])
+                total_cost += (shares * price) 
+            except (IndexError, ValueError) as e: 
+                print("Couldn't parse", line)
+    return total_cost
+    
+print('Total cost', portfolio_cost('Data/portfolio.csv'))
