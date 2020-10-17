ff73e2110ab0334ccd00508e6770ee784978953c
Ben Trybulski <btrybulski1@gmail.com>
2020-10-17 11:59:33 -0400

    add formatted report table output to report.py

--- Work/report.py
+++ Work/report.py
@@ -32,6 +32,16 @@ def read_prices(filename):
                 print('Unable to parse row', row)
     return portfolio
 
+def make_report(portfolio, prices):
+    report = []
+    for record in portfolio: 
+        name = record['name']
+        shares = record['shares']
+        price = record['price']
+        change = prices[name] - price
+        report.append((name, shares, price, change))
+    return report
+
 if __name__ == "__main__":
     portfolio = read_portfolio('Data/portfolio.csv')
     prices = read_prices('Data/prices.csv')
@@ -46,3 +56,12 @@ if __name__ == "__main__":
     print('Total cost', total_cost)
     print('Total value', total_value)
     print('Profit', round(total_value - total_cost, ndigits=2))
+    print()
+
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print(f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
+    print('---------- ---------- ---------- -----------')
+    report = make_report(portfolio, prices)
+    for name, shares, price, change in report:
+        price = '${:.2f}'.format(price)
+        print(f'{name:>10s} {shares:>10d} {price:>10s} {change:>10.2f}')
