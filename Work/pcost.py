d47f8153b37490805ce9c7ee86a41e00d3f379ec
Ben Trybulski <btrybulski1@gmail.com>
2020-10-17 11:17:02 -0400

    use csv and cmd args for running pcost

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,18 +2,45 @@
 #
 # Exercise 1.27
 
+#def portfolio_cost(filename):
+#    total_cost = 0
+#    with open(filename, 'rt') as file: 
+#        # csv heading - stock name, number of shares, purchase price
+#        headers = next(file)
+#        for line in file: 
+#            row = line.split(',')
+#            try: 
+#                ticker, shares, price = row[0], int(row[1]), float(row[2])
+#                total_cost += (shares * price) 
+#            except (IndexError, ValueError) as e: 
+#                print("Couldn't parse", line)
+#    return total_cost
+#    
+#print('Total cost', portfolio_cost('Data/portfolio.csv'))
+
+# Exercise 1.32
+
+import csv 
+import sys 
+
 def portfolio_cost(filename):
     total_cost = 0
     with open(filename, 'rt') as file: 
-        # csv heading - stock name, number of shares, purchase price
-        headers = next(file)
-        for line in file: 
-            row = line.split(',')
+        rows = csv.reader(file)
+        headers = next(rows)
+        for row in rows: 
             try: 
-                ticker, shares, price = row[0], int(row[1]), float(row[2])
-                total_cost += (shares * price) 
-            except (IndexError, ValueError) as e: 
-                print("Couldn't parse", line)
+                nshares = int(row[1])
+                price = float(row[2])
+                total_cost += (price * nshares)
+            except (ValueError, IndexError):
+                print("Couldn't parse", row)
     return total_cost
-    
-print('Total cost', portfolio_cost('Data/portfolio.csv'))
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else: 
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
