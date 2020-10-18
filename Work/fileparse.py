fbafb2a67542fbdcd26a21899b87e9772a52f8cf
Ben Trybulski <btrybulski1@gmail.com>
2020-10-18 14:20:15 -0400

    add general csv reading function

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,30 @@
 # fileparse.py
 #
 # Exercise 3.3
+import csv
+
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
+    """Parse a csv file into a list of records"""
+    with open(filename) as f:
+        rows = csv.reader(f, delimiter=delimiter)
+        records = []
+        indices = []
+        if has_headers:
+            headers = next(rows)
+            if select:
+                indices = [headers.index(colname) for colname in select]
+                headers = select
+        for row in rows:
+            if not row:
+                continue
+            if indices:
+                row = [row[index] for index in indices]
+            if types:
+                row = [func(val) for func, val in zip(types, row)]
+            if has_headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
+            records.append(record)
+
+    return records
