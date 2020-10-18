8fd31308b091699e49fb808984ce1ef1aa93bac6
Ben Trybulski <btrybulski1@gmail.com>
2020-10-18 18:19:05 -0400

    improve error handling for parse csv

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,8 +3,10 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
     """Parse a csv file into a list of records"""
+    if select and not has_headers:
+        raise RuntimeError("select argument requires column headers")
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimiter)
         records = []
@@ -14,17 +16,21 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
             if select:
                 indices = [headers.index(colname) for colname in select]
                 headers = select
-        for row in rows:
+        for ind, row in enumerate(rows, start=1):
             if not row:
                 continue
             if indices:
                 row = [row[index] for index in indices]
             if types:
-                row = [func(val) for func, val in zip(types, row)]
+                try:
+                    row = [func(val) for func, val in zip(types, row)]
+                except ValueError as e:
+                    if not silence_errors:
+                        print("Row", ind, ":", "Couldn't convert", row)
+                        print(e)
             if has_headers:
                 record = dict(zip(headers, row))
             else:
                 record = tuple(row)
             records.append(record)
-
     return records
